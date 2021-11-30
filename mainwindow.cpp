3ae9f75513f1a5a3d7d44cdfebc5e087e6fa146d
taniamathew <tmtaniamary@gmail.com>
2021-11-29 21:04:52 -0500

    hellooooo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,14 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+    QBrush brushGray(Qt::gray);
+    QBrush brushDarkGray(Qt::darkGray);
+    QPen penBlack(Qt::black);
+    penBlack.setWidth(6);
+
+    rectangle = scene->addRect(10, 10, 202, 272, penBlack, brushGray);
 }
 
 MainWindow::~MainWindow()
