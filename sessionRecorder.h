7de2379d86913947936006e91d13f120987cec86
Steeveyboy <jonathan.qsteeves@gmail.com>
2021-12-08 21:45:14 -0500

    housekeeping

--- sessionRecorder.h
+++ sessionRecorder.h
@@ -7,27 +7,15 @@
 #include "record.h"
 #include <fstream>
 /* Class Purpose: stores sessions in a vector and reads into a text file
- * Data Members:
- *      int numRecords;
-        std::vector<record*> records;
-        std::string filepath = "/home/student/CES-Simulation/COMP_3004_CES_SIM/records.txt";
-        std::string date;
- *
- * Class Functions:
- *      void storeRecords();
-        void initRecords();
-        void printRecords();
-        std::vector<record*> getHistory();
-        void deleteRecord(int);
-        void makeRecord(std::string, std::string, int, std::string, std::string);
- */
+*/
 class sessionRecorder {
 
-
 	private:
+        //Adjust the filepath to your system, to ensure records are stored.
+        std::string filepath = "/home/student/CES-Simulation/COMP_3004_CES_SIM/records.txt";
+
 		int numRecords;
 		std::vector<record*> records;
-        std::string filepath = "/home/student/CES-Simulation/COMP_3004_CES_SIM/records.txt";
 		std::string date; //possibly an ENUM
 		void storeRecords();
 		void initRecords();
